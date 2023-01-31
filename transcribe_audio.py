ac7f7907150f891da9204d1815496c931fda4bd9
Zachary Keyes <zachk@arizona.edu>
2023-01-31 11:27:56 -0700

    need to add readme

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,14 +1,42 @@
 '''
-Author: Zachary Keyes
+    Author: Zachary Keyes
 
-This script uses OpenAI Whisper's API (https://github.com/openai/whisper/blob/main/README.md#python-usage)
-to transcribe the audio file from prep_audio.py. It also puts it into a .txt file.
+    This Python script uses OpenAI Whisper
+    (https://github.com/openai/whisper/blob/main/README.md#python-usage)
+    to transcribe the audio file from prep_audio.py.
+    It then puts it into a .txt file.
 '''
-# make sure whisper is installed; if not, install it with: pip install whisper
-import whisper # see https://github.com/openai/whisper for documentation
+
+# Make sure these packages are installed.
+# Install them both from the command line with:
+# $ pip install pydub whisper
+from pydub import AudioSegment #  https://github.com/jiaaro/pydub#quickstart
+import whisper #  https://github.com/openai/whisper#python-usage
+
+# Use the location of the audio file you want to transcribe.
+input_file_name = "interiew.mp3"
+
+# use your own file's file extension (mine was "mp3")
+sound = AudioSegment.from_file(input_file_name, format="mp3") # load source
 
 
-# use the location of the *prepped* audio file you want to use.
+# don't change these, these stay the same!
+sound = sound.set_channels(1)
+sound = sound.set_frame_rate(16000)
+
+# set desired output file; best if it's not the same as input_file_name.
+output_file_name = "/Users/zachary/Desktop/FSHD/interiew_prepped.mp3"
+
+# This will create the file in the desired format (whisper works best with mp3 and wav formats,
+# I believe) and put all of the audio data inside of it.
+sound.export(output_file_name, format="mp3")
+
+# make sure whisper is installed; if not, install it with:
+# $ pip install whisper
+
+
+
+# use the location of the prepped audio file you want to use.
 input_file_name = "/Users/zachary/Desktop/FSHD/interiew_prepped.mp3"
 
 # this loads the language model (we want to use "base", so don't change that!) that will
