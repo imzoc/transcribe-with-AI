0fcad6cb2b3be5f6b838379fe4efe8e7505573f3
Zachary Keyes <zachk@arizona.edu>
2023-01-31 11:59:58 -0700

    update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,66 +1,33 @@
 '''
     Author: Zachary Keyes
+    Class: FSHD 150B1
+    Description: This Python script uses OpenAI Whisper and PyDub
+    to transcribe the audio file from prep_audio.py. It then puts it
+    into a .txt file. 
 
-    This Python script uses OpenAI Whisper
-    (https://github.com/openai/whisper/blob/main/README.md#python-usage)
-    to transcribe the audio file from prep_audio.py.
-    It then puts it into a .txt file.
+    If you're a newbie, see README.md
+    If you're also curious and want a little extra explanation, also
+    see README.md.
+
 '''
 
-# Make sure these packages are installed.
-# Install them both from the command line with:
-# $ pip install pydub whisper
-from pydub import AudioSegment #  https://github.com/jiaaro/pydub#quickstart
-import whisper #  https://github.com/openai/whisper#python-usage
+from pydub import AudioSegment
+import whisper
 
-# Use the location of the audio file you want to transcribe.
-input_file_name = "interiew.mp3"
+### USING PYDUB (https://github.com/jiaaro/pydub#quickstart)
+### Most file formats will work as long as they have audio in them
+### somewhere. This section preps the audio so that whisper will work
+### most effectively. Replace "interiew.m4a" with the file you want to
+### transcribe and "m4a" with the appropriate file extension.
 
-# use your own file's file extension (mine was "mp3")
-sound = AudioSegment.from_file(input_file_name, format="mp3") # load source
+stream = AudioSegment.from_file("interiew.m4a", "m4a")  # Modify this!
+stream.set_channels(1)  # Don't modify this!
+stream.set_frame_rate(16000)  # Don't modify this!
+stream.export("prepped.mp3", format="mp3")  # Don't modify this!
 
-
-# don't change these, these stay the same!
-sound = sound.set_channels(1)
-sound = sound.set_frame_rate(16000)
-
-# set desired output file; best if it's not the same as input_file_name.
-output_file_name = "/Users/zachary/Desktop/FSHD/interiew_prepped.mp3"
-
-# This will create the file in the desired format (whisper works best with mp3 and wav formats,
-# I believe) and put all of the audio data inside of it.
-sound.export(output_file_name, format="mp3")
-
-# make sure whisper is installed; if not, install it with:
-# $ pip install whisper
-
-
-
-# use the location of the prepped audio file you want to use.
-input_file_name = "/Users/zachary/Desktop/FSHD/interiew_prepped.mp3"
-
-# this loads the language model (we want to use "base", so don't change that!) that will
-# transcribe the audio file. This is the "AI" part.
-model = whisper.load_model("base") # load model
-
-# this transcribes the audio file with the loaded language model.
-RESULT = model.transcribe(input_file_name) # transcribe audio file
-
-# RESULT now stores the transcribed text. We're going to put it into a file.
-
-# this creates and opens a file to write onto. Use the location and name of the .txt file you want.
-# Don't touch the "w", that just tells open() to use write mode instead of read mode or something.
-t_file = open("/Users/zachary/Desktop/FSHD/transcription.txt", "w") # create a .txt file
-
-# now we write the transcribed text. 
-t_file.write(RESULT["text"]) # write transcribed text to file
-
-# it's good practice to close files when you're done with them.
-t_file.close() # close file
-
-''' A little bit of extra explanation...
-
-RESULT here is actually a python dictionary object. To access anything stored in a dictionary,
-you have to use a "key", which then returns a "value". model.transcribe(input_file_name) stores
-the transcribed text that we want under the "text" key. 
-'''
+### USING WHISPER (https://github.com/openai/whisper#python-usage)
+model = whisper.load_model("base")  # the "base" model should be sufficient.
+transcribed_audio = model.transcribe("prepped.mp3")  # Don't modify this!
+transcription = open("transcription.txt", "w")  # This creates the text file
+transcription.write(transcribed_audio["text"])  # This writes the text file
+transcription.close()  # This closes the text file (good practice)
