593882a18d6373a13e296bb9b101918a4aba373a
Zachary Keyes <zachk@arizona.edu>
2023-01-31 11:51:47 -0700

    removed extra files

--- prep_audio.py
+++
@@ -1,8 +0,0 @@
-'''
-Author: Zachary Keyes
-
-This script prepares the audio file for the language model we want to use. Specifically,
-it just changes the number of channels to 1 if it was recorded in stereo mode or something and sets
-the frame rate/frequency to 16000Hz.
-'''
-
